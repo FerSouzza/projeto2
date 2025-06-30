142b05caf36df006ec6d3056ae7647cb2f4b5fab
Fernando Souza <138416145+FerSouzza@users.noreply.github.com>
2025-06-30 10:08:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function jogoUm() {
+  window.location.href = "file:///C:/Users/ferna/Desktop/projeto%202/exemplometodolista/index.html";
+}
+
+function jogoDois() {
+  window.location.href = "file:///C:/Users/ferna/Desktop/projeto%202/formulario/index.html";
+}
+
+function jogoTres() {
+  window.location.href = "file:///C:/Users/ferna/Desktop/projeto%202/JogoNumeroSecreto/index.html";
+}
