0242e6030a7bb4b8c3902063f708f682d4eb2e41
Fernando Souza <138416145+FerSouzza@users.noreply.github.com>
2025-07-02 16:59:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,7 @@ function carregarJogo(nomeJogo) {
   const pasta = `jogos/${nomeJogo}`;
 
   conteudo.innerHTML = `
-    <iframe src="${JogoNumeroSecreto}/index.html"
-            width="100%" height="600"
-            style="border:1px solid #ccc;">
-    </iframe>`;
-}
-  conteudo.innerHTML = `
-    <iframe src="${exemplometodolista}/index.html"
-            width="100%" height="600"
-            style="border:1px solid #ccc;">
-    </iframe>`;
-}
-  conteudo.innerHTML = `
-    <iframe src="${formulario}/index.html"
+    <iframe src="${pasta}/index.html"
             width="100%" height="600"
             style="border:1px solid #ccc;">
     </iframe>`;
