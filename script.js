1fa7e978a03436600c435de47cbeea9a1ca920cd
Fernando Souza <138416145+FerSouzza@users.noreply.github.com>
2025-07-02 16:27:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,26 @@
 function jogoUm() {
-  let h1 = document.querySelector("h1");
-  h1.textContent = "Você escolheu Manipular Frutas!";
+  document.querySelector("h1").textContent = "Você escolheu Manipulando Frutas!";
 
-  let divGif = document.querySelector(".gif");
-  divGif.innerHTML = "<p>Exemplo de frutas:</p><ul><li>Maçã</li><li>Banana</li><li>Uva</li></ul>";
+  document.getElementById("conteudo").src = 
+    "https://fersouzza.github.io/projeto2/exemplometodolista/index.html";
+
+  document.querySelector(".gif").innerHTML = "";
 }
 
 function jogoDois() {
-  let h1 = document.querySelector("h1");
-  h1.textContent = "Você escolheu Formulário de Valores!";
+  document.querySelector("h1").textContent = "Você escolheu Formulário de Valores!";
 
-  let divGif = document.querySelector(".gif");
-  divGif.innerHTML = `
-    <form>
-      <label>Digite um valor:</label>
-      <input type="text" id="valor" />
-      <button type="button" onclick="mostrarValor()">Enviar</button>
-    </form>
-  `;
+  document.getElementById("conteudo").src = 
+    "https://fersouzza.github.io/projeto2/formulario/index.html";
+
+  document.querySelector(".gif").innerHTML = "";
 }
 
 function jogoTres() {
-  let h1 = document.querySelector("h1");
-  h1.textContent = "Você escolheu Número Secreto!";
+  document.querySelector("h1").textContent = "Você escolheu Número Secreto!";
 
-  let divGif = document.querySelector(".gif");
-  divGif.innerHTML = `
-    <p>Tente adivinhar o número secreto (1 a 10):</p>
-    <input type="number" id="chute" />
-    <button type="button" onclick="verificarNumero()">Chutar</button>
-    <p id="mensagem"></p>
-  `;
-}
+  document.getElementById("conteudo").src = 
+    "https://fersouzza.github.io/projeto2/JogoNumeroSecreto/index.html";
 
-function mostrarValor() {
-  let valor = document.querySelector("#valor").value;
-  alert("Você digitou: " + valor);
-}
-
-function verificarNumero() {
-  let chute = parseInt(document.querySelector("#chute").value);
-  let numeroSecreto = 7;
-
-  let mensagem = document.querySelector("#mensagem");
-
-  if (chute === numeroSecreto) {
-    mensagem.textContent = "Parabéns! Você acertou!";
-  } else {
-    mensagem.textContent = "Tente novamente!";
-  }
+  document.querySelector(".gif").innerHTML = "";
 }
